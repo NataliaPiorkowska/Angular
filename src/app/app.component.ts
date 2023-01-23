c0e5c96bcbaceecc2b9c95eadc43f1aa5af59deb
NatiDelta <natalia.piorkowska@seargin.com>
2023-01-23 20:42:26 +0100

    created components and its base logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { resetFakeAsyncZone } from '@angular/core/testing';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  username: any;
+
+  reset(){
+   this.username="";
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { RecipesComponent } from './recipes/recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipes/recipe-list/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipes/recipe-detail/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipes/recipe-list/recipe-list/recipe-item/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-list/shopping-edit/shopping-edit/shopping-edit.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
